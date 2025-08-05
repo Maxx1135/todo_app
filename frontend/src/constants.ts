79adef6d37870041cd50327e44a3aaec9bb9d9e8
Douzy11 <mahery@fluxbuild.io>
2025-08-05 17:56:20 +0300

    updates of the project

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -2,3 +2,8 @@
 export const homePage = "/home";
 export const loginPage = "/login";
 export const logoutPage = "/logout";
+
+// Configs
+export const VITE_PUBLIC_SUPABASE_ANON_KEY = import.meta.env
+  .VITE_PUBLIC_SUPABASE_ANON_KEY as string;
+export const VITE_SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL! as string;
